4ca366540323cdd300b8fd67d8591369b3f389a0
Phuong Nguyen <Phuong_Nguyen@epam.com>
2021-10-28 12:27:44 +0700

    add test cases

--- src/test/java/com/phuong/e3homework/selenium4/Selenium4Test.java
+++ src/test/java/com/phuong/e3homework/selenium4/Selenium4Test.java
@@ -1,6 +1,7 @@
 package com.phuong.e3homework.selenium4;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,7 +22,6 @@ public class Selenium4Test extends SeleniumBaseTest {
         return driver;
     }
 
-    //1
     @Test
     public void testOpenGoogle(){
         WebDriver driver = openGoogle();
@@ -29,12 +29,11 @@ public class Selenium4Test extends SeleniumBaseTest {
         driver.quit();
     }
 
-    //2
     @Test
     public void testInputSearchVisible(){
         WebDriver driver = openGoogle();
 
-        WebElement inputElement = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+        WebElement inputElement = driver.findElement(By.name("q"));
 
         Assert.assertTrue(inputElement.isDisplayed());
         Assert.assertTrue(inputElement.isEnabled());
@@ -42,53 +41,85 @@ public class Selenium4Test extends SeleniumBaseTest {
         driver.quit();
     }
 
-    //3
     @Test
     public void testInputSearchAction(){
         WebDriver driver = openGoogle();
 
-        WebElement inputElement = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+        WebElement inputElement = driver.findElement(By.xpath("//input[@class = 'gLFyf gsfi']"));
 
         inputElement.sendKeys("epam");
-        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]")).click();
+        driver.findElement(By.name("btnK")).submit();
 
         Assert.assertTrue(driver.getCurrentUrl().contains("https://www.google.com/search?q=epam"));
         driver.quit();
     }
 
-    //4
+    @Test
+    public void testInputSearchActionEnter(){
+        WebDriver driver = openGoogle();
+
+        WebElement inputElement = driver.findElement(By.xpath("//input[@class = 'gLFyf gsfi']"));
+
+        inputElement.sendKeys("epam");
+        driver.findElement(By.xpath("//input[@class = 'gLFyf gsfi']")).sendKeys(Keys.ENTER);
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.google.com/search?q=epam"));
+        driver.quit();
+    }
+
     @Test
     public void testDoodlesButton(){
         WebDriver driver = openGoogle();
-        WebElement doodlesButton = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[2]"));
+        WebElement doodlesButton = driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']/center/input[2]"));
         doodlesButton.click();
-        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.google.com/doodles"));
-        driver.quit();
+        Assert.assertTrue(driver.getCurrentUrl().contains("google.com/doodles"));
+        driver.close();
     }
 
-    //5
     @Test
-    public void testSignIn(){
+    public void testImagesButton(){
         WebDriver driver = openGoogle();
-        WebElement signInButton = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div/div/div[2]/a"));
-        signInButton.click();
-        Assert.assertTrue(driver.getCurrentUrl().contains("https://accounts.google.com/signin/"));
+        WebElement imagesButton = driver.findElement(By.xpath("//*[@id=\"gb\"]/div/div[1]/div/div[2]/a"));
+        imagesButton.click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("google.com.vn/imghp"));
         driver.quit();
     }
 
-    //5
     @Test
-    public void testForgotEmail(){
+    public void testGmailButton(){
         WebDriver driver = openGoogle();
-        WebElement forgotEmailButton = driver.findElement(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[3]/button"));
-        forgotEmailButton.click();
-        Assert.assertTrue(driver.getCurrentUrl().contains("https://accounts.google.com/signin/v2/usernamerecovery"));
+        WebElement imagesButton = driver.findElement(By.xpath("//*[@id=\"gb\"]/div/div[1]/div/div[1]/a"));
+        imagesButton.click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("gmail"));
         driver.quit();
     }
 
-    //6
     @Test
-    public void test(){
-
+    public void testAboutButton(){
+        WebDriver driver = openGoogle();
+        WebElement imagesButton = driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[1]/a[1]"));
+        imagesButton.click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("about.google/?utm_source"));
+        driver.quit();
     }
+
+    @Test
+    public void testAdvertisingButton(){
+        WebDriver driver = openGoogle();
+        WebElement imagesButton = driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[1]/a[2]"));
+        imagesButton.click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("ads.google.com/intl"));
+        driver.quit();
+    }
+
+    @Test
+    public void testBusinessButton(){
+        WebDriver driver = openGoogle();
+        WebElement imagesButton = driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[1]/a[3]"));
+        imagesButton.click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("business"));
+        driver.quit();
+    }
+
+
 }
